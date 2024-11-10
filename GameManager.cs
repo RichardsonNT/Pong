4b886949c2c90436cdc1d08d3ceb1d3c6ed0cbac
Noah Richardson <44816634+RichardsonNT@users.noreply.github.com>
2024-11-10 14:03:03 -0500

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,49 @@
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro.Examples;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+ 
+
+public Ball ball;
+public Paddle playerPaddle;
+public Paddle computerPaddle;
+
+[SerializeField] TextMeshProUGUI playerScoreText;
+[SerializeField] TextMeshProUGUI computerScoreText;
+//public Text playerScoreText;
+//public Text computerScoreText;
+
+private int _playerScore;
+private int _computerScore;
+
+public void PlayerScores()
+{
+    _playerScore++;
+
+        this.playerScoreText.text = _playerScore.ToString();
+        ResetRound();
+}
+public void ComputerScores()
+{
+    _computerScore++;
+
+        this.computerScoreText.text = _computerScore.ToString();
+        ResetRound();
+}
+
+    private void ResetRound()
+    {
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+
+    
+
+}
+
